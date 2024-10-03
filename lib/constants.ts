cc8579ced1baea92a3fa06b7fdec2992811a690f
Justin Duke <justin@buttondown.email>
2024-10-03 15:33:37 -0400

    Add comment explaining above

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,4 @@
+// The `www` is deliberate; as of this writing, we explicitly redirect from the
+// root domain to the `www` subdomain, so a `shovel.report` canonical URL would be
+// incorrect (since it would 308 to the `www` subdomain).
 export const CANONICAL_URL = "https://www.shovel.report";
